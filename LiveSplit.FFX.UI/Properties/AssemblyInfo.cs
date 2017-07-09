da55595d4ae2c73b972ca3f14e18e21a82b4f6e7
flobberworm4 <lukeroddis@hotmail.com>
2017-07-10 00:04:06 +0100

    Bug fix - Missing factory
    When activating the splits in the Edit Splits section, it would throw an exception because there was no factory set on the component. This seems to fix it?

--- LiveSplit.FFX.UI/Properties/AssemblyInfo.cs
+++ LiveSplit.FFX.UI/Properties/AssemblyInfo.cs
@@ -4,7 +4,6 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComponentFactory(typeof(FFXUIFactory))]
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
